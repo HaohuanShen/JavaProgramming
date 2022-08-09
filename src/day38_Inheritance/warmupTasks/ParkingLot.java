99f3779249b21d59b120b4a700f2f686d3cf4ca6
HaohuanShen <107886850+HaohuanShen@users.noreply.github.com>
2022-08-09 21:31:48 +0200

    create ParkingLot

---
+++ src/day38_Inheritance/warmupTasks/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.warmupTasks;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camery",2020,20000,"Gray",123456);
+
+        Tesla tesla= new Tesla("model s ",2019,25000,"white",50000);
+
+        BMW bmw= new BMW("X5",2020,67350,"Black",12000);
+
+    }
+}
