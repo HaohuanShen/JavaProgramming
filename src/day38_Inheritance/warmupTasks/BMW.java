acaa076b7c6a751f9b7b3521d6717c97031afbb9
HaohuanShen <107886850+HaohuanShen@users.noreply.github.com>
2022-08-09 16:31:29 +0200

    update Toyota

--- src/day38_Inheritance/warmupTasks/BMW.java
+++ src/day38_Inheritance/warmupTasks/BMW.java
@@ -1,5 +1,18 @@
 package day38_Inheritance.warmupTasks;
 
-public class BMW {
+public class BMW extends Car {
+
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breaksDown() {
+        System.out.println(brand + " " + model + " is breaking down");
+    }
+
+    public void race() {
+        System.out.println(brand + " " + model + " is racing");
+    }
+
 
 }
