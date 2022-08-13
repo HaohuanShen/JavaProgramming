029e8c398cadd42555722220873079d0ecbd74c9
HaohuanShen <107886850+HaohuanShen@users.noreply.github.com>
2022-08-13 17:20:40 +0200

    Square and rectangle

--- src/day39_Recap/sharpTask/Square.java
+++ src/day39_Recap/sharpTask/Square.java
@@ -21,6 +21,23 @@ public class  Square extends Sharp{
         setSide(side);
     }
 
+    @Override
+    public double area() {
+        return side*side;
+    }
 
+    @Override
+    public double perimeter() {
+        return 4*side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", perimeter=" + perimeter() +
+                ", area=" + area() +
+                '}';
+    }
 }
 
