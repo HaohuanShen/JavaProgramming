6d937f457dcf291534c970b6e6c53104a6c9de3b
HaohuanShen <107886850+HaohuanShen@users.noreply.github.com>
2022-08-13 22:30:24 +0200

    Circle

---
+++ src/day39_Recap/sharpTask/Circle.java
@@ -0,0 +1,44 @@
+package day39_Recap.sharpTask;
+
+public class Circle extends Sharp{
+
+    public static double pi = 3.14;
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+
+        if(radius<=0){
+            System.err.println("Invalid radius: "+radius);
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+
+    public Circle( double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+    @Override
+    public double area() {
+        return pi*radius*radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*pi*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", area=" + area() +
+                ", perimeter=" + perimeter() +
+                '}';
+    }
+}
