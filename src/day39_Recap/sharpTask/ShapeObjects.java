4d3707fb3d46fed82f1511b1ccd3c66c58845c4b
HaohuanShen <107886850+HaohuanShen@users.noreply.github.com>
2022-08-14 08:53:36 +0200

    ShapeObjects

---
+++ src/day39_Recap/sharpTask/ShapeObjects.java
@@ -0,0 +1,22 @@
+package day39_Recap.sharpTask;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(7.5,3.50);
+
+        System.out.println("rectangle = " + rectangle);
+
+        Circle circle = new Circle(3);
+
+        System.out.println("circle = " + circle);
+
+        Square square = new Square(4.5);
+
+        System.out.println("square = " + square);
+
+
+    }
+
+}
