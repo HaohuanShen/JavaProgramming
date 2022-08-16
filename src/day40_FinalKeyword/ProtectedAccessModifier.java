7c637ad9d29a5974dee3e6a82dca6a0b3e1826a8
HaohuanShen <107886850+HaohuanShen@users.noreply.github.com>
2022-08-16 08:15:57 +0200

    Final Keyword

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,16 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1 = "Cydeo";// default access modifier===>only visible in the same package
+    protected static String name2 = "WoodenSpoon";// protected access modifier===>visible in all the subclasses
+
+
+    static void method1(){
+
+    }
+
+    protected static void method2(){
+
+    }
+}
